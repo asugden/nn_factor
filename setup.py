53003ac160a17f7eca8a234d3c44e9c27e47bceb
Arthur <arthur.sugden@gmail.com>
2025-06-19 22:37:23 -0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import platform
+
+from setuptools import setup
+
+# Run with python setup.py develop --user
+
+setup_requires = []
+
+scripts = []
+# scripts.extend(glob('scripts/*py'))
+
+
+CLASSIFIERS = """\
+Development Status :: 4 - Beta
+Intended Audience :: Science/Research
+License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)
+Operating System :: MacOS
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Programming Language :: Python
+Topic :: Scientific/Engineering
+"""
+setup(
+    name="nn_factor",
+    version="0.1",
+    packages=["nn_factor"],
+    #   scripts = [''],
+    #
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=[
+        "numpy>=1.22.4",
+        "pandas>=1.4.2",
+    ],
+    scripts=scripts,
+    author="Arthur Sugden",
+    author_email="sugdena@duq.edu",
+    description="Pure math models for partitions",
+    setup_requires=setup_requires,
+    # setup_requires=['setuptools_cython'],
+    url="https://allref.io",
+    platforms=["Linux", "Mac OS-X", "Windows"],
+    ext_modules=[],
+)
